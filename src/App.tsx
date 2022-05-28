89e090465d5802857717c55c7f8cb9f0297d01e8
piltom <22397970+piltom@users.noreply.github.com>
2022-05-28 11:45:14 +0200

    fix: automatic linter fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import * as React from "react"
+import * as React from 'react';
 import {
   ChakraProvider,
   Box,
   Heading,
-} from "@chakra-ui/react"
-import { theme } from "./theme/theme"
+} from '@chakra-ui/react';
+import { theme } from './theme/theme';
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box>
-      <Heading>
-        Cereal Management System
-      </Heading>
-    </Box>
-  </ChakraProvider>
-)
+export function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box>
+        <Heading>
+          Cereal Management System
+        </Heading>
+      </Box>
+    </ChakraProvider>
+  );
+}
