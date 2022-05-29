700c31275476f2b9a995e392670578e340143ad2
takihama <danielcalderonuns@gmail.com>
2022-05-29 09:13:07 -0300

    add: header to raws

--- src/pages/RawMaterials.tsx
+++ src/pages/RawMaterials.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Stack } from '@chakra-ui/react';
+import { IconType } from 'react-icons';
+import { GiWoodPile } from 'react-icons/gi';
+import { BsPlus } from 'react-icons/bs';
+import { BiImport } from 'react-icons/bi';
+import Header from '../components/header/Header';
 
+interface HeaderButtons {
+  name: string
+  icon?: IconType
+  onClick: React.MouseEventHandler<Element>
+}
+interface RawMaterialsState {
+  searchRawMaterials: string
+}
 export default function RawMaterials() {
+  const [searchRawMaterials, setSearchRawMaterials] = useState<RawMaterialsState['searchRawMaterials']>('');
+  const onSearchChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchRawMaterials(evt.target.value);
+  };
+  const handleCreateRawMaterial = () => {
+    console.log('handleCreateRawMaterial');
+  };
+  const handleImportRawMaterials = () => {
+    console.log('handleImportRawMaterials');
+  };
+  const headerButtons: Array<HeaderButtons> = [{
+    name: 'Create',
+    icon: BsPlus,
+    onClick: handleCreateRawMaterial,
+  },
+  {
+    name: 'Import',
+    icon: BiImport,
+    onClick: handleImportRawMaterials,
+  },
+  ];
   return (
-    <div>
-      RawMaterials
-    </div>
+    <Stack width="full">
+      <Header
+        title="Raws"
+        icon={GiWoodPile}
+        search={{
+          name: 'searchProduct',
+          value: searchRawMaterials,
+          placeholder: 'Filter by raw material',
+          onChange: onSearchChange,
+        }}
+        buttons={headerButtons}
+      />
+    </Stack>
   );
 }
