cd7ddaa6a58bc0d3b55540aefc818acf80237369
takihama <danielcalderonuns@gmail.com>
2022-05-29 09:38:27 -0300

    add: header to dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Stack } from '@chakra-ui/react';
+import { MdDashboard } from 'react-icons/md';
+import Header from '../components/header/Header';
 
 export default function Dashboard() {
   return (
-    <div>
-      Dashboard
-    </div>
+    <Stack width="full">
+      <Header
+        title="Dashboard"
+        icon={MdDashboard}
+        search={null}
+        buttons={[]}
+      />
+    </Stack>
   );
 }
