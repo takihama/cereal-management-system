9e306d2bcde378bc108ae948380cf252392ac02f
takihama <danielcalderonuns@gmail.com>
2022-05-29 10:42:35 -0300

    add: oee cards to dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,81 @@
 import React from 'react';
-import { Stack } from '@chakra-ui/react';
+import { Stack, Text } from '@chakra-ui/react';
 import { MdDashboard } from 'react-icons/md';
 import Header from '../components/header/Header';
+import OEECard from '../components/card/OEECard';
 
 export default function Dashboard() {
+  const oee = {
+    title: { name: 'Overall OEE', value: 84.15, unit: '%' },
+    data: [{
+      name: 'Availability',
+      value: 91.93,
+      unit: '%',
+    },
+    {
+      name: 'Performance',
+      value: 96.36,
+      unit: '%',
+    },
+    {
+      name: 'Quality',
+      value: 95,
+      unit: '%',
+    },
+    ],
+    color: 'primary.800',
+  };
+  const availability = {
+    title: { name: 'Availability', value: 91.93, unit: '%' },
+    data: [{
+      name: 'Planned runtime',
+      value: 3840,
+      unit: 'mins',
+    },
+    {
+      name: 'Actual runtime',
+      value: 3530,
+      unit: 'mins',
+    },
+    {
+      name: 'Unplanned downtime',
+      value: 310,
+      unit: 'mins',
+    },
+    ],
+    color: 'orange.800',
+  };
+  const performance = {
+    title: { name: 'Performance', value: 96.36, unit: '%' },
+    data: [{
+      name: 'Planned quality',
+      value: 81000,
+      unit: '',
+    },
+    {
+      name: 'Actual quality',
+      value: 78050,
+      unit: '',
+    },
+    ],
+    color: 'green.800',
+  };
+  const quality = {
+    title: { name: 'Quality', value: 96.36, unit: '%' },
+    data: [{
+      name: 'Actual quality',
+      value: 78050,
+      unit: '',
+    },
+    {
+      name: 'Rejected quality',
+      value: 3900,
+      unit: '',
+    },
+    ],
+    color: 'cyan.800',
+  };
+
   return (
     <Stack width="full">
       <Header
@@ -12,6 +84,38 @@ export default function Dashboard() {
         search={null}
         buttons={[]}
       />
+      <Stack
+        direction="row"
+        justify="space-between"
+        width="full"
+      >
+        <Text>Hola</Text>
+        <Stack
+          paddingX="4"
+          spacing="8"
+        >
+          <OEECard
+            title={oee.title}
+            data={oee.data}
+            color={oee.color}
+          />
+          <OEECard
+            title={availability.title}
+            data={availability.data}
+            color={availability.color}
+          />
+          <OEECard
+            title={performance.title}
+            data={performance.data}
+            color={performance.color}
+          />
+          <OEECard
+            title={quality.title}
+            data={quality.data}
+            color={quality.color}
+          />
+        </Stack>
+      </Stack>
     </Stack>
   );
 }
