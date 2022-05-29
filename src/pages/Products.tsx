fe8c762cf35c7a8cc6a04a861150fb89e1c5b315
takihama <danielcalderonuns@gmail.com>
2022-05-28 21:34:01 -0300

    add: header to products

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Stack } from '@chakra-ui/react';
+import { FaBoxOpen } from 'react-icons/fa';
+import { BsPlus } from 'react-icons/bs';
+import { BiImport } from 'react-icons/bi';
+import { IconType } from 'react-icons';
+import Header from '../components/header/Header';
 
+interface HeaderButtons {
+  name: string
+  icon?: IconType
+  onClick: React.MouseEventHandler<Element>
+}
+
+interface ProductsState {
+  searchProduct: string
+}
 export default function Products() {
+  const [searchProduct, setSearchProduct] = useState<ProductsState['searchProduct']>('');
+  const onSearchChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchProduct(evt.target.value);
+  };
+  const onCreateProduct = () => {
+    console.log('Create product');
+  };
+  const onImportProducts = () => {
+    console.log('Import products');
+  };
+  const buttons: Array<HeaderButtons> = [{
+    name: 'Create',
+    icon: BsPlus,
+    onClick: onCreateProduct,
+  },
+  {
+    name: 'Import',
+    icon: BiImport,
+    onClick: onImportProducts,
+  },
+  ];
   return (
-    <div>
-      Products
-    </div>
+    <Stack
+      width="100%"
+    >
+      <Header
+        title="Products"
+        icon={FaBoxOpen}
+        search={{
+          name: 'searchProduct',
+          value: searchProduct,
+          placeholder: 'Filter by product name or SKU',
+          onChange: onSearchChange,
+        }}
+        buttons={buttons}
+      />
+    </Stack>
   );
 }
