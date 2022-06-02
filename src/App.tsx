ea210482503169fe13d10b0731af17389c61e12c
takihama <danielcalderonuns@gmail.com>
2022-06-02 00:21:10 -0300

    refactor: main routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
 import * as React from 'react';
-import {
-  ChakraProvider,
-  Stack,
-} from '@chakra-ui/react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Stack } from '@chakra-ui/react';
+import { Outlet } from 'react-router-dom';
 import { Sidebar } from './components/sidebar/Sidebar';
-import Dashboard from './pages/Dashboard';
-import extTheme from './theme/theme';
-import Production from './pages/Production';
-import Products from './pages/Products';
-import RawMaterials from './pages/RawMaterials';
 
 export default function App() {
   return (
-    <ChakraProvider theme={extTheme}>
-      <BrowserRouter>
-        <Stack direction="row" spacing="0">
-          <Sidebar />
-          <Routes>
-            <Route path="/" element={<h1>Home</h1>} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/production" element={<Production />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/rawmaterials" element={<RawMaterials />} />
-          </Routes>
-        </Stack>
-      </BrowserRouter>
-    </ChakraProvider>
+    <Stack direction="row" spacing="0">
+      <Sidebar />
+      <Outlet />
+    </Stack>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,29 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
+import extTheme from './theme/theme';
+import Dashboard from './pages/Dashboard';
+import Production from './pages/Production';
+import Products from './pages/Products';
+import RawMaterials from './pages/RawMaterials';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <App />,
+  <ChakraProvider theme={extTheme}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/production" element={<Production />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/rawmaterials" element={<RawMaterials />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </ChakraProvider>,
 );
