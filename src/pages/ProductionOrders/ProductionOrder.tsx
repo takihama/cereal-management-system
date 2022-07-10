6a5e35ae53ea1622482cdb7eb74c5824baa3811e
takihama <danielcalderonuns@gmail.com>
2022-07-09 23:27:07 -0300

    add: start production order button

--- src/pages/ProductionOrders/ProductionOrder.tsx
+++ src/pages/ProductionOrders/ProductionOrder.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
-import { Stack } from '@chakra-ui/react';
+import { Box, Stack } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import { AiOutlineTool } from 'react-icons/ai';
+import { BsPlayFill } from 'react-icons/bs';
 import Header from '../../components/header/Header';
 
 export default function ProductionOrder() {
   const params = useParams();
+  const handleStartProductionOrder = () => {
+    console.log('start production order');
+  };
+  const headerButtons = [{
+    name: 'Start',
+    icon: BsPlayFill,
+    onClick: handleStartProductionOrder,
+  }];
   return (
     <Stack width="full">
       <Header
         title={`Production Order: ${params.productionOrderId}`}
         icon={AiOutlineTool}
-        buttons={[]}
+        buttons={headerButtons}
       />
+      <Box padding="4">
+        <Box bg="white" padding="4">
+          {params.productionOrderId}
+        </Box>
+      </Box>
     </Stack>
   );
 }
