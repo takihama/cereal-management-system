f61e8e78d4fdf0ccd9c25ce26dc04ac2318df95b
takihama <danielcalderonuns@gmail.com>
2022-07-11 16:37:51 -0300

    add: config for deployment

--- cereal-management-system-api/cereal-management-system-api/cereal-management-system-api/cereal-management-system-api/src/index.ts
+++ cereal-management-system-api/cereal-management-system-api/cereal-management-system-api/cereal-management-system-api/src/index.ts
@@ -1,15 +1,22 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
+
 import productsRouter from './controllers/products';
 import productionOrdersRouter from './controllers/productionOrders';
 import rawsRouter from './controllers/raws';
 
-import cors from 'cors';
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 3001;
+const publicPath = path.join(__dirname, '..', 'build');
+app.use(express.static(publicPath));
+
+const PORT = process.env.PORT || 3001;
 
 app.get('/ping', (_req, res) => {
   console.log('Someone pinged here');
